b5edea0a0a3338d368fa62301e3044a26c5e8a06
harshitmandada1 <55108110+harshitmandada1@users.noreply.github.com>
2019-11-13 16:38:51 -0500

    adding title strings

--- api/models/Todo.js
+++ api/models/Todo.js
@@ -2,12 +2,43 @@ const mongoose = require('mongoose');
 
 // Define schema for todo items
 const todoSchema = new mongoose.Schema({
-  name: {
+
+  title: {
     type: String,
-  },
-  done: {
-    type: Boolean,
-  },
+    required: "title is required"
+},
+/**
+ * Todo created date.
+ */
+createdDate: {
+    type: Date,
+    default: Date.now
+},
+/**
+ * Todo content.
+ */
+content: {
+    type: String
+},
+/**
+ * Last modified date.
+ */
+modifiedDate: {
+    type: Date,
+    default: Date.now
+}
+}, {
+versionKey: false
+});
+
+// Duplicate the id field as mongoose returns _id field instead of id.
+todoSchema.virtual('id').get(function(){
+  return this._id.toHexString();
+});
+
+// Ensure virtual fields are serialised.
+todoSchema.set('toJSON', {
+  virtuals: true
 });
 
 const Todo = mongoose.model('Todo', todoSchema);
